f5720ae4b6bb891a57ddeccf46b3359572f7e1e2
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2018-11-19 04:10:36 +0000

    Create collection_widget.dart

---
+++ lib/collection_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CollectionWidget extends StatefulWidget {
+  @override
+  CollectionWidgetApp createState() => new CollectionWidgetApp();
+}
+
+class CollectionWidgetApp extends State<CollectionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('stock').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(record.item),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.item),
+          trailing: Text(record.instock.toString()),
+          onTap: () =>
+              record.reference.updateData({'instock': record.instock + 1}),
+          onLongPress: () =>
+              record.reference.updateData({'instock': record.instock - 1}),
+        ),
+      ),
+    );
+  }
+}
+
+class Record {
+  final String item;
+  final int instock;
+  final DocumentReference reference;
+
+  @override
+  String toString() => "Record<$item:$instock>";
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['item'] != null),
+        assert(map['instock'] != null),
+        item = map['item'],
+        instock = map['instock'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+}
